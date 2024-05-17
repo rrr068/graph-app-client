27a7acb2c47e9ea9e131031a6cf19db53c8f49ef
山田燎弥 <ryoya068@yamadaniwabiwatarunoMacBook-Air.local>
2024-05-18 00:15:27 +0900

    サインアップ機能　着手

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -14,6 +14,7 @@ import { useState } from 'react'
 import { useMutation } from '@apollo/client';
 import { SignInResponse } from '../types/signInResponse';
 import { SIGN_IN } from '../mutations/authMutations';
+import { useNavigate } from 'react-router-dom';
 
 const theme = createTheme();
 
@@ -22,6 +23,7 @@ export default function SignIn() {
   const [password, setPassword] = useState('');
   const [failSignIn, setFailSignIn] = useState(false);
   const [signIn] = useMutation<SignInResponse>(SIGN_IN);
+  const navigate = useNavigate();
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -30,9 +32,17 @@ export default function SignIn() {
       const result = await signIn({
         variables: {signInInput}
       });
-      console.log(result);
+      if (result.data) {
+        localStorage.setItem('token', result.data.signIn.accessToken);
+      }
+      localStorage.getItem('token') && navigate('/')
     } catch(err: any) {
+      if (err.message === 'Unauthorized') {
+        setFailSignIn(true);
+        return;
+      }
       console.log(err.message);
+      alert('予期せぬエラー');
     }
   };
 
@@ -79,6 +89,9 @@ export default function SignIn() {
               value={password}
               onChange={(e) => {setPassword(e.target.value)}}
             />
+            { failSignIn && <Typography color='red'>
+              メールアドレスまたはパスワードを確認してください。
+              </Typography>}
             <Button
               type="submit"
               fullWidth
